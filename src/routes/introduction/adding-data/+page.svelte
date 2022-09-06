dcec1db7c57cd7bfca297447adb1079692f31a09
AkitoTsukahara <a-tsukahara@macloud.jp>
2022-09-07 01:44:53 +0900

    introduction pages

---
+++ src/routes/introduction/adding-data/+page.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+  let name = 'Svelte'
+</script>
+<h1>Hello {name} !!!</h1>
---
+++ src/routes/introduction/dynamic-attributes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  let src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png'
+  let name = 'pikachu'
+</script>
+
+<img src={src} alt={name}>
