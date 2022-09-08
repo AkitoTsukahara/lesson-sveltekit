5d0f7043a41b8536fa2bdebd957bf6a1bd42ac9d
AkitoTsukahara <a-tsukahara@macloud.jp>
2022-09-09 02:11:43 +0900

    lesson reactivity

---
+++ src/routes/reactive/statements/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  let count = 0;
+
+  // 任意の処理をリアクティブに発火させることができる
+  $: if (count >= 10) {
+    alert('count is dangerously high!');
+    count = 9;
+  }
+
+  function handleClick() {
+    count += 1;
+  }
+</script>
+
+<button on:click={handleClick}>
+  Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
