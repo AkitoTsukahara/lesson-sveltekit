5cf5dd4d8c50110c5d1f1dc8afcbd4d741e247df
AkitoTsukahara <a-tsukahara@macloud.jp>
2022-09-14 00:23:06 +0900

    base popper

---
+++ src/routes/popper/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import type { Placement } from '@popperjs/core'
+  import TextTip from './TextTip.svelte'
+
+  let placementRef: HTMLElement | undefined = undefined
+  export let placement: Placement = 'top'
+  export let text = 'TOOLTIPS'
+</script>
+
+<div class="parent">
+  {#if placementRef}
+    <TextTip {text} parentElementRef={placementRef} {placement} />
+  {/if}
+  <div class="test-element" bind:this={placementRef}>Sample Popper</div>
+</div>
+
+<style lang="scss">
+  .parent {
+    padding: 100px;
+    width: fit-content;
+  }
+  .test-element {
+    font-size: 40px;
+    width: fit-content;
+  }
+</style>
--- src/routes/popper/TextTip.svelte
+++ src/routes/popper/TextTip.svelte
@@ -1,21 +1,10 @@
 <script lang="ts">
-  import Close from '../icon/close/Close.svelte'
-  import { createEventDispatcher } from 'svelte'
   import { createPopper, type Placement } from '@popperjs/core'
-  import Portal from 'svelte-portal'
-  import { sameWidthModifier } from '$lib/package/dom/popperModifiers'
 
   export let text = ''
   let ref: HTMLElement | undefined = undefined
   export let parentElementRef: HTMLElement
   export let placement: Placement = 'top'
-  export let usePortal = false
-  export let useSameWidthModifier = false
-
-  const dispatch = createEventDispatcher()
-  function close() {
-    dispatch('closed')
-  }
 
   $: if (parentElementRef && ref) {
     createPopper(parentElementRef, ref, {
@@ -27,31 +16,17 @@
             offset: [0, 8]
           }
         },
-        ...(useSameWidthModifier ? ([sameWidthModifier] as const) : [])
       ]
     })
   }
 </script>
 
-{#if usePortal}
-  <Portal>
-    <div class="tool-tip" bind:this={ref} data-popper-placement={placement}>
-      <div class="text">
-        {@html text.replace(/\n/g, '<br>')}
-      </div>
-      <div class="close-icon" on:click={close}><Close /></div>
-      <div data-popper-arrow class="arrow" />
-    </div>
-  </Portal>
-{:else}
-  <div class="tool-tip" bind:this={ref} data-popper-placement={placement}>
-    <div class="text">
-      {@html text.replace(/\n/g, '<br>')}
-    </div>
-    <div class="close-icon" on:click={close}><Close /></div>
-    <div data-popper-arrow class="arrow" />
+<div class="tool-tip" bind:this={ref} data-popper-placement={placement}>
+  <div class="text">
+    {@html text.replace(/\n/g, '<br>')}
   </div>
-{/if}
+  <div data-popper-arrow class="arrow" />
+</div>
 
 <style lang="scss">
   .tool-tip {
@@ -66,7 +41,7 @@
       font-weight: 400;
       font-size: 14px;
       line-height: 20px;
-      color: $text-white;
+      color: #fff;
       padding: 16px;
     }
 
@@ -86,16 +61,6 @@
     .arrow::before {
       content: '';
       transform: rotate(45deg);
-    }
-
-    .close-icon {
-      display: flex;
-      width: 12px;
-      height: 12px;
-      position: absolute;
-      top: 6px;
-      right: 6px;
-      cursor: pointer;
     }
   }
 </style>
